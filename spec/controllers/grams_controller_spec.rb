6320e45005bee02a4a5e675c28490b7524b4fcb4
Risse Rigdon <risse.rigdon@gmail.com>
2016-03-31 02:36:58 +0000

    Add associations between users and grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -11,6 +11,13 @@ RSpec.describe GramsController, type: :controller do
 
   describe "grams#new action" do
     it "should successfully show the new form" do
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )
+      sign_in user
+
       get :new
       expect(response).to have_http_status(:success)
     end
@@ -18,17 +25,33 @@ RSpec.describe GramsController, type: :controller do
 
   describe "grams#create action" do
     it "should successfully create a gram in the database" do
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )
+      sign_in user
+      
       post :create, gram: {message: 'Hello!'}
       expect(response).to redirect_to root_path
 
       gram = Gram.last
       expect(gram.message).to eq("Hello!")
+      expect(gram.user).to eq(user)
     end
 
     it "should properly deal with validation errors" do
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )
+      sign_in user
+
+      gram_count = Gram.count
       post :create, gram: {message: ''}
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(Gram.count).to eq 0
+      expect(gram_count).to eq Gram.count
     end
 
   end
